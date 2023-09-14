b703b6e606ee3f2d7dbae878f49ee850131be792
TheFaheem <35933338+FaheemOnHub@users.noreply.github.com>
2023-09-14 15:57:27 +0530

    Add files via upload

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,16 @@
-'use strict';
+// 'use strict';
+// let age = 30;
+// let oldAge = age;
+// age = 31;
+// console.log(oldAge);
+// console.log(age);
+
+// const me = {
+//   name: 'Faheem',
+//   age: 30,
+// };
+
+// const friend = me;
+// friend.age = 27; //changed only friends age
+// console.log('Friend:', friend);
+// console.log('Me', me);
